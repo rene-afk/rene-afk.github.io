8057144bd99799dbf69babf22a48b01bb63c4ac4
Aravind Krishnan A <krishnanaravind8702@gmail.com>
2021-05-25 12:09:13 +0530

    github hosting trial 2

--- Art_Portfolio_Website/js/script.js
+++ Art_Portfolio_Website/js/script.js
@@ -1,6 +1,6 @@
-//$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+//$(function () { // Same as document.addEventLis../..ner("DOMCon../..ntLoaded"../Art_Portfolio_Websi../...
 
-  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+  // Same as document.querySelector("#navbarToggle").addEventLis../..ner("blur",../Art_Portfolio_Websi../...
   //$("#navbarToggle").blur(function (event) {
     //var screenWidth = window.innerWidth;
     //if (screenWidth < 768) {
@@ -35,39 +35,39 @@ var threeDimentionalArt = "snippets/3D.html";
 
 // Convenience function for inserting innerHTML for 'select'
 var insertHtml = function (selector, html) {
-  var targetElem = document.querySelector(selector);
-  targetElem.innerHTML = html;
+  var targe../..lem = document.querySelector(selector);
+  targe../..lem.innerHTML = html;
 };
 
 // Show loading icon inside element identified by 'selector'.
 var showLoading = function (selector) {
-  var html = "<div class='text-center'>";
+  var html = "<div class='../..xt-cen../..r'>";
   html += "<img src='images/ajax-loader.gif'></div>";
   insertHtml(selector, html);
 };
 
 // On page load (before images or CSS)
-document.addEventListener("DOMContentLoaded", function (event) {
+document.addEventLis../..ner("DOMCon../..ntLoaded", function (event) {
 
 // On first load, show home view
-showLoading("#main-content");
+showLoading("#main-con../..nt");
 $ajaxUtils.sendGetRequest(
   homeHtml,
-  function (responseText) {
-    document.querySelector("#main-content")
-      .innerHTML = responseText;
+  function (response../..xt) {
+    document.querySelector("#main-con../..nt")
+      .innerHTML = response../..xt;
   },
   false);
 });
 
 //Explorations Home load into index.html
 dc.loadExplorationsHome = function(){
-  showLoading("#main-content");
+  showLoading("#main-con../..nt");
   $ajaxUtils.sendGetRequest(
     explorationsHome,
-    function (responseText) {
-    document.querySelector("#main-content")
-      .innerHTML = responseText;
+    function (response../..xt) {
+    document.querySelector("#main-con../..nt")
+      .innerHTML = response../..xt;
   },
   false);
 }
@@ -75,12 +75,12 @@ dc.loadExplorationsHome = function(){
 //Projects page load into index.html
 
 dc.loadProjects = function(){
-  showLoading("#main-content");
+  showLoading("#main-con../..nt");
   $ajaxUtils.sendGetRequest(
     projects,
-    function (responseText) {
-    document.querySelector("#main-content")
-      .innerHTML = responseText;
+    function (response../..xt) {
+    document.querySelector("#main-con../..nt")
+      .innerHTML = response../..xt;
   },
   false);
 }
@@ -88,46 +88,46 @@ dc.loadProjects = function(){
 //About me page load into indexhtml
 
 dc.loadAboutMe = function(){
-  showLoading("#main-content");
+  showLoading("#main-con../..nt");
   $ajaxUtils.sendGetRequest(
     aboutMe,
-    function (responseText) {
-    document.querySelector("#main-content")
-      .innerHTML = responseText;
+    function (response../..xt) {
+    document.querySelector("#main-con../..nt")
+      .innerHTML = response../..xt;
   },
   false);
 }
 
 dc.loadMixedMedia = function(){
-  showLoading("#main-content");
+  showLoading("#main-con../..nt");
   $ajaxUtils.sendGetRequest(
     mixedMediaArt,
-    function (responseText) {
-    document.querySelector("#main-content")
-      .innerHTML = responseText;
+    function (response../..xt) {
+    document.querySelector("#main-con../..nt")
+      .innerHTML = response../..xt;
   },
   false);
 }
 
 dc.loadDigital = function(){
-  showLoading("#main-content");
+  showLoading("#main-con../..nt");
   $ajaxUtils.sendGetRequest(
     digitalArt,
-    function (responseText) {
-    document.querySelector("#main-content")
-      .innerHTML = responseText;
+    function (response../..xt) {
+    document.querySelector("#main-con../..nt")
+      .innerHTML = response../..xt;
   },
   false);
 }
 
 
 dc.load3D = function(){
-  showLoading("#main-content");
+  showLoading("#main-con../..nt");
   $ajaxUtils.sendGetRequest(
     threeDimentionalArt,
-    function (responseText) {
-    document.querySelector("#main-content")
-      .innerHTML = responseText;
+    function (response../..xt) {
+    document.querySelector("#main-con../..nt")
+      .innerHTML = response../..xt;
   },
   false);
 }
@@ -146,7 +146,7 @@ integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
 crossorigin="anonymous">
 
 $(function(){
-      $("#header").load("../decorations/header.html"); 
-      $("#footer").load("../decorations/footer.html");
+      $("#header").load("../../Art_Portfolio_Website/decorations/header.html"); 
+      $("#footer").load("../../Art_Portfolio_Website/decorations/footer.html");
 });
 
